2030376ff95a680a0ff44fe6ac76c363de64578a
Mr. Cat <trananhmanh89@gmail.com>
2017-04-05 22:30:42 +0700

    Third commit
    Third commit

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,10 +1,4 @@
 <?php 
-/**
- * @package     Content Tag Merging for Joomla
- * @subpackage  plg_sys_tagmerging
- * @copyright   Copyright (C) MrMeo - D4J Team http://designforjoomla.com
- * @license     GPLv2 or later
- */
 
 defined('_JEXEC') or die;
 ?>
